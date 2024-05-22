044014da9fe4be99e8db0390e9d3256e99137d03
willr4in <113215579+willr4in@users.noreply.github.com>
2024-05-22 17:23:33 +0300

    Create Tree.fs

---
+++ soop/Tree.fs
@@ -0,0 +1,53 @@
+﻿module Tree
+
+open System
+
+
+type stype = string
+
+
+type op =
+    | Plus | Minus | Times | Divide
+    | LessThan | GreaterThan
+
+
+type expr =
+    | Int of int
+    | String of string
+    | Binop of op * expr * expr
+    | Var of string
+    // methodName, args
+    | MethodCall of expr * expr list
+
+type stmt =
+    | NewAssign of stype * string * expr
+    | Assign of string * expr
+    // condition, ifpart, elsepart
+    | If of expr * stmt list * stmt list
+    // condition, loop code
+    | While of expr * stmt list
+    | DoExpr of expr
+
+type accessModifier =
+    | Public | Private | Protected
+
+// name, type, initial value
+type arg = Arg of string * stype
+
+// name, params, return type, stmts
+type methodDecl = Method of string * arg list * stype * stmt list
+
+type classDecl = 
+    | LocalVar of string * accessModifier * stype * expr
+    | Method of string * accessModifier * arg list * stype * stmt list
+    | Constructor of accessModifier * arg list * stmt list
+
+type classInterfaceDecl = 
+    // local vars, constructors, methods 
+    | Class of string * classDecl list
+    // methods
+    | Interface of methodDecl list
+
+type namespaceDecl = Namespace of string * classInterfaceDecl list
+
+type program = Program of namespaceDecl list
